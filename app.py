4ca2c89d1e2bb79f9e6eb7caf651129bc69339ef
bondar-aleksandr <54150127+bondar-aleksandr@users.noreply.github.com>
2021-12-16 19:37:07 +0200

    implemented get/post for item

--- app.py
+++ app.py
@@ -1,78 +1,38 @@
 import werkzeug.exceptions
 from flask import Flask, jsonify, request
+from flask_restful import Api, Resource
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
-stores = [
-    {
-        'name': 'store_01',
-        'items': [
-            {
-                'name': 'item01',
-                'price': 10
-            }
-        ]
-    }
+items = [
 ]
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/store', methods = ['POST'])
-def create_store():
-    try:
-        req_data = request.get_json()
-        new_store = {
-            'name': req_data['name'],
-            'items': []
-        }
-    except KeyError:
-        return jsonify({'result': 'error: no name provided!'})
-    except werkzeug.exceptions.BadRequest:
-        return jsonify({'result': 'Wrong request format!'})
+class Item(Resource):
+    def get(self, name):
+        for item in items:
+            if item['name'] == name:
+                return item
+        return {'status': 'no such item!'}, 404
 
 
-    stores.append(new_store)
-    return jsonify({'result':'Success'})
+    def post(self, name):
+        try:
+            price = request.get_json()['price']
+        except KeyError:
+            return {'status': 'error - no price provided!'}, 400
+        item = {'name': name, 'price': price}
+        items.append(item)
+        return item, 201
+
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
 
 
-@app.route('/store/<string:name>')
-def get_store(name):
-    for store in stores:
-        if store['name'] == name:
-            return jsonify(store)
-    return jsonify({'result':f'error - No such store as {name}'})
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
 
-
-@app.route('/store')
-def get_stores():
-    return jsonify({'stores':stores})
-
-
-@app.route('/store/<string:name>/item', methods = ['POST'])
-def create_item_in_store(name):
-    try:
-        item_name = request.get_json()['name']
-        item_price = request.get_json()['price']
-    except werkzeug.exceptions.BadRequest as e:
-        return jsonify({'result': f'error: {e}'})
-    except KeyError:
-        return jsonify({'result': 'not enough info provided!'})
-    for store in stores:
-        if store['name'] == name:
-            item = {
-                'name': item_name,
-                'price': item_price
-            }
-            store['items'].append(item)
-            return jsonify(item)
-    return jsonify({'result':'store not found'})
-
-
-@app.route('/store/<string:name>/item')
-def get_items_in_store(name):
-    for store in stores:
-        if store['name'] == name:
-            return jsonify({'items':store['items']})
-    return jsonify({'result':f'error - No such store as {name}'})
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+app.run(debug=True)
