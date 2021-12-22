af1ff996559d42c2af4fb44ab6efd7b78e19f562
bondar-aleksandr <54150127+bondar-aleksandr@users.noreply.github.com>
2021-12-22 14:23:17 +0200

    added delete, put methods

--- app.py
+++ app.py
@@ -17,11 +17,11 @@ api = Api(app)
 jwt = JWT(app=app, authentication_handler=authenticate, identity_handler=identity)
 
 class Item(Resource):
-    @jwt_required()
     def get(self, name):
         item = filter(lambda x: x['name'] == name, items)
         return next(item, None), 200 if item else 404
 
+    # @jwt_required()
     def post(self, name):
         if next(filter(lambda x: x['name'] == name, items), None):
             return {'message': f'item {name} already exists!'}, 400
@@ -32,6 +32,28 @@ class Item(Resource):
         item = {'name': name, 'price': price}
         items.append(item)
         return item, 201
+
+    # @jwt_required()
+    def delete(self, name):
+        global items
+        if next(filter(lambda x: x['name'] == name, items), None):
+            items = list(filter(lambda x: x['name'] != name, items))
+            return {'message': 'item deleted!'}
+        return {'message': 'no such item!'}
+
+    # @jwt_required()
+    def put(self, name):
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item:
+            try:
+                price = request.get_json()['price']
+            except KeyError:
+                return {'status': 'error - no price provided!'}, 400
+            item.update({'name':name, 'price': price})
+            return item, 201
+        else:
+            return self.post(name)
+
 
 class ItemList(Resource):
     def get(self):
